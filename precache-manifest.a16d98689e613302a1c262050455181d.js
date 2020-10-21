6a330da09b6f950f4f08bc2c2d81b800f5e9c056
FREEZ2385 <schlee9703@naver.com>
2020-10-21 19:33:56 +0900

    Updates

---
+++ precache-manifest.a16d98689e613302a1c262050455181d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128a472ce5823bb67fbb7d0e6b39fe25",
+    "url": "/ko-react/index.html"
+  },
+  {
+    "revision": "7657cc62531a7bdf0bd6",
+    "url": "/ko-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df95f2987ac0360a477a",
+    "url": "/ko-react/static/js/2.fa03bae9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/ko-react/static/js/2.fa03bae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7657cc62531a7bdf0bd6",
+    "url": "/ko-react/static/js/main.2c8f80f7.chunk.js"
+  },
+  {
+    "revision": "6cd4656de9ea41f905bf",
+    "url": "/ko-react/static/js/runtime-main.8c6c98c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ko-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
